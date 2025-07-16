903697ccef13662e92f2c3a2f3634714acc7eb3e
Fernando Lozoya <fernandolv1995@gmail.com>
2025-07-16 13:54:32 -0600

    First commit.

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ChatPage from './components/ChatPage';
+import socketIO from 'socket.io-client';
+
+const socket = socketIO.connect('http://localhost:4000');
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
