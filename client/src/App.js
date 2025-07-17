73d9f80d4359cb0647ad1610bd4d51b039fa3bba
Fernando Lozoya <fernandolv1995@gmail.com>
2025-07-17 16:02:18 -0600

    Connection fix.

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ const getSocketUrl = () => {
 
   return isLocalhost
     ? 'http://localhost:4000'
-    : 'https://lozoya.org:4000';
+    : 'https://lozoya.org';
 };
 
 const socket = socketIO.connect(getSocketUrl());
